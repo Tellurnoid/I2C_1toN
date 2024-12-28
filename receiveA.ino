c699dae1733a3a7a7d3e2673a3616e1aca27fe41
Tellurnoid <105495041+Tellurnoid@users.noreply.github.com>
2024-12-28 18:23:01 +0900

    Add files via upload

---
+++ receiveA.ino
@@ -0,0 +1,23 @@
+//cf) https://stemship.com/arduino-beginner-i2c/
+
+#include "Wire.h"
+
+#define LED 13
+volatile byte receiveValue;       
+
+void setup() {
+  Wire.begin(8); //自分のアドレスを自由に設定。宛先に使われる 
+  Wire.onReceive(dataReceive);      
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  Serial.println(receiveValue);
+  if(receiveValue==16){digitalWrite(LED, HIGH);}else{digitalWrite(LED, LOW);}
+}
+
+void dataReceive(int Number) {        
+  if(Wire.available())        
+    receiveValue = Wire.read();       
+}
---
+++ sender.ino
@@ -0,0 +1,21 @@
+//cf) https://stemship.com/arduino-beginner-i2c/
+
+#include "Wire.h"        
+
+void setup() {
+  Wire.begin();        
+}
+
+int valFor2 = 16;
+int valFor3 = 18;   
+
+void loop() {      
+  Wire.beginTransmission(8); //アドレス2のボードに送信       
+  Wire.write(valFor2);//1バイト(＝8ビット)単位でのデータ送信しかできないため、8ビット(0～255)の範囲   
+  Wire.endTransmission();        
+  delay(500);
+  Wire.beginTransmission(9); //アドレス2のボードに送信 
+  Wire.write(valFor3);        
+  Wire.endTransmission();        
+  delay(500);
+}
