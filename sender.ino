b306cc3592340d7392c20c2b815154525dc14b47
Tellurnoid <105495041+Tellurnoid@users.noreply.github.com>
2024-12-28 20:38:03 +0900

    sender.ino の更新

--- sender.ino
+++ sender.ino
@@ -7,14 +7,14 @@ void setup() {
 }
 
 int valFor2 = 16;
-int valFor3 = 18;   
+int valFor3 = 18;   //好きな値を
 
 void loop() {      
-  Wire.beginTransmission(8); //アドレス2のボードに送信       
+  Wire.beginTransmission(8); //アドレス8のボードに送信       
   Wire.write(valFor2);//1バイト(＝8ビット)単位でのデータ送信しかできないため、8ビット(0～255)の範囲   
   Wire.endTransmission();        
   delay(500);
-  Wire.beginTransmission(9); //アドレス2のボードに送信 
+  Wire.beginTransmission(9); //アドレス9のボードに送信 
   Wire.write(valFor3);        
   Wire.endTransmission();        
   delay(500);
